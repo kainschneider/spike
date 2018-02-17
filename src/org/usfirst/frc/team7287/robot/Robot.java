6ad963418f2d23015696c47e026983ce5f1765e1
Esq Atom Smashers <ezekielc0535@sd61learn.ca>
2018-02-16 19:11:01 -0500

    Added initial auto-drive code. the robot will now drive forward for 2 seconds at 100%

--- src/org/usfirst/frc/team7287/robot/Robot.java
+++ src/org/usfirst/frc/team7287/robot/Robot.java
@@ -10,6 +10,7 @@ package org.usfirst.frc.team7287.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
@@ -19,15 +20,33 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class Robot extends IterativeRobot {
 	private DifferentialDrive m_myRobot;
 	private Joystick m_leftStick;
-	private Joystick m_rightStick;
-
+	Timer timer;
+	
+	
 	@Override
 	public void robotInit() {
 		m_myRobot = new DifferentialDrive(new Spark(0), new Spark(1));
 		m_leftStick = new Joystick(0);
+		timer = new Timer();
+	}
+	
+	@Override
+	public void autonomousInit() {
+
+		timer.reset();
+		timer.start();
+	}
+	
+	public void autonomousPeriodic() {
+		//come back to timer and change time etc
+		if (timer.get() <2.0) {
+			m_myRobot.tankDrive(1.0,1.0);
+		} else {
+			m_myRobot.tankDrive(0.0,0.0 );
+		
+		}
 		
 	}
-
 	@Override
 	public void teleopPeriodic() {
 		m_myRobot.arcadeDrive(-m_leftStick.getY()*(0.4*m_leftStick.getRawAxis(3)+0.6), m_leftStick.getRawAxis(4));
