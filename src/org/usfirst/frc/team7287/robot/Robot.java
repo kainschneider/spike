895e37747eb3e059e1e05ed755f644364b9cd5ef
Esq Atom Smashers <ezekielc0535@sd61learn.ca>
2018-02-17 17:58:06 -0500

    Calibration of robot is now a thing.

--- src/org/usfirst/frc/team7287/robot/Robot.java
+++ src/org/usfirst/frc/team7287/robot/Robot.java
@@ -21,7 +21,10 @@ public class Robot extends IterativeRobot {
 	private DifferentialDrive m_myRobot;
 	private Joystick m_leftStick;
 	Timer timer;
-	
+	boolean shouldRamp = false;
+	double turnSpeed = .4425;
+	double linearSpeed = 0.5;
+	int timeFactor;
 	
 	@Override
 	public void robotInit() {
@@ -32,23 +35,80 @@ public class Robot extends IterativeRobot {
 	
 	@Override
 	public void autonomousInit() {
-
+		timeFactor = 1;
 		timer.reset();
 		timer.start();
 	}
 	
+	
+	@Override
 	public void autonomousPeriodic() {
-		//come back to timer and change time etc
-		if (timer.get() <2.0) {
-			m_myRobot.tankDrive(0.5, 0.5, true);
+		if (timer.get() < timeFactor+2 && timer.get() > timeFactor + 1.0) {
+			forward(linearSpeed);
+		} else if (timer.get() > timeFactor && timer.get() < timeFactor + 0.5) {
+			turn("left",turnSpeed);
+			stop();
+		} else if (timer.get()> timeFactor + 0.5 && timer.get() < timeFactor+1.0) {
+			stop();
 		} else {
-			m_myRobot.tankDrive(0.0, 0.0, true);
-		
+			stop();
+			timeFactor++;
 		}
-		
+		calibrate();
 	}
+	
+	private void calibrate() {
+		if (timer.get() < 1.0) {
+			forward(linearSpeed);
+		} else if (timer.get() > 1.0 && timer.get() < 1.5) {
+			turn("left", turnSpeed); 
+		} else if (timer.get()> 1.5 && timer.get() < 2.0) {
+			stop();
+		} else if (timer.get() > 2.0 && timer.get() < 3.0) {
+			forward(linearSpeed);
+		} else if (timer.get() > 3.0 && timer.get() < 3.5) {
+			turn("left", turnSpeed);
+		} else if (timer.get() > 4.0 && timer.get() < 5.0) {
+			forward(linearSpeed);
+		} else if (timer.get() > 5.0 && timer.get() < 5.5) {
+			turn("left", turnSpeed);
+		} else if (timer.get() > 6.0 && timer.get() < 7.0) {
+			forward(linearSpeed);
+		} else if (timer.get() > 7.0 && timer.get() < 7.5) {
+			turn("left", turnSpeed);
+		} else {
+			stop();
+		}
+	}
+	
+	private void forward(double speed) {
+		System.out.print("Forward with speed " + speed);
+		m_myRobot.tankDrive(speed, speed, shouldRamp);
+	}
+	
+	private void stop() {
+		System.out.print("Stop nuff said");
+		m_myRobot.tankDrive(0.0, 0.0, shouldRamp);
+	}
+	
+	private void reverse(double speed) {
+		System.out.print("Reverse with speed " + speed);
+		m_myRobot.tankDrive(-speed, -speed, shouldRamp);
+	}
+	
+	private void turn(String direction, double speed) {
+		System.out.print("Turn with direction "+ direction + "and speed " + speed);
+		switch(direction.toLowerCase()) {
+		case "right": 
+			m_myRobot.tankDrive(speed, -speed, shouldRamp);
+		case "left":
+			m_myRobot.tankDrive(-speed, speed, shouldRamp);
+		default:
+		}
+	}
+	
 	@Override
 	public void teleopPeriodic() {
-		m_myRobot.arcadeDrive(-m_leftStick.getY()*(0.4*m_leftStick.getRawAxis(3)+0.6), m_leftStick.getRawAxis(4));
+		m_myRobot.arcadeDrive(-m_leftStick.getY()*(0.4*m_leftStick.getRawAxis(3)+0.6), m_leftStick.getRawAxis(4)*(0.4*m_leftStick.getRawAxis(3)+0.6));
 	}
 }
