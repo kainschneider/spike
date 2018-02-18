cb5faf58970c5e6b099e8c029c025d3abc1d90a6
Esq Atom Smashers <ezekielc0535@sd61learn.ca>
2018-02-18 16:19:09 -0500

    The programming team called a reverse function and confirmed it's existence and optimized robot stop and cube drop time.

--- src/org/usfirst/frc/team7287/robot/Robot.java
+++ src/org/usfirst/frc/team7287/robot/Robot.java
@@ -1,22 +1,14 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 
 package org.usfirst.frc.team7287.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
+
 public class Robot extends IterativeRobot {
 	private DifferentialDrive m_myRobot;
 	private Joystick m_leftStick;
@@ -45,19 +37,22 @@ public class Robot extends IterativeRobot {
 	
 	@Override
 	public void autonomousPeriodic() {
+		initialCubeDrop();
+	}
+	
+	private void initialCubeDrop() {
 		double driveTime = 2.5;
 		double superSpeed = 1.0;
-		if (timer.get() < driveTime * 0.2) {
+		double fallTime = 0.25;
+		if (timer.get() < driveTime * 0.1) {
 			forward(superSpeed);
-		} else if(timer.get() < driveTime * 0.4 && timer.get() > driveTime * 0.2) {
+		} else if(timer.get() < driveTime * fallTime && timer.get() > driveTime * 0.1) {
 			stop(); 
-		} else if(timer.get() > driveTime * 0.4 && timer.get() < driveTime * 1.0) {
+		} else if(timer.get() > driveTime * fallTime && timer.get() < driveTime * 1.0) {
 			forward(0.5);
 		} else {
 			stop();
 		}
-		
-		
 	}
 	
 	private void calibrate() {
@@ -76,22 +71,22 @@ public class Robot extends IterativeRobot {
 
 	
 	private void forward(double speed) {
-		System.out.print("Forward with speed " + speed);
+		System.out.println("Forward with speed " + speed);
 		m_myRobot.tankDrive(speed, speed, shouldRamp);
 	}
 	
 	private void stop() {
-		System.out.print("Stop nuff said");
+		System.out.println("Stop nuff said");
 		m_myRobot.tankDrive(0.0, 0.0, shouldRamp);
 	}
 	
 	private void reverse(double speed) {
-		System.out.print("Reverse with speed " + speed);
+		System.out.println("Reverse with speed " + speed);
 		m_myRobot.tankDrive(-speed, -speed, shouldRamp);
 	}
 	
 	private void turn(String direction, double speed) {
-		System.out.print("Turn with direction "+ direction + "and speed " + speed);
+		System.out.println("Turn with direction "+ direction + "and speed " + speed);
 		switch(direction.toLowerCase()) {
 		case "right": 
 			m_myRobot.tankDrive(speed, -speed, shouldRamp);
